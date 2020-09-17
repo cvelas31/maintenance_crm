03306b0fd748b4e6d6d8dcc35ce54714d88a20cf
Camilo Velasquez A <cvelas31@gmail.com>
2020-09-17 00:23:47 -0500

    Both databases

--- src/settings.py
+++ src/settings.py
@@ -82,8 +82,12 @@ WSGI_APPLICATION = 'src.wsgi.application'
 
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'demo_test',
+#         'USER': 'postgres',
+#         'PASSWORD': 'q12345678',
+#         'HOST': 'localhost',
+#         'PORT': 5432,
 #     }
 # }
 DATABASES = {
